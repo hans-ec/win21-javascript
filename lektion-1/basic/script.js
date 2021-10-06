d62d9777bc0b204e0ac55ab81eb57505fea8e37f
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2021-10-06 11:53:36 +0200

    lektion 1

---
+++ lektion-1/basic/script.js
@@ -0,0 +1,9 @@
+// DOM - Document Object Model
+document.getElementById("div-1").innerText = "<h2>Detta är div-1</h2>"
+document.getElementById("div-2").innerHTML = "<h2>Detta är div-2</h2>"
+
+
+// console
+console.log(document.getElementById("div-2").innerText)
+console.warn(document.getElementById("div-2").innerHTML)
+console.error("Skriver ut saker i consolen")
